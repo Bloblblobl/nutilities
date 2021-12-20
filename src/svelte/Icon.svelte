5bdfd83314cdf0baa5564d7c5deee43e9181fbfc
Saar Sayfan <saarsayfan@gmail.com>
2021-12-19 23:10:44 -0800

    add icons.svg and Icon component

---
+++ src/svelte/Icon.svelte
@@ -0,0 +1,9 @@
+<script>
+    export let name;
+    export let width = '1.5rem';
+    export let height = '1.5rem';
+</script>
+
+<svg class={$$props.class} {width} {height}>
+    <use href={`../img/icons.svg#${name}`} />
+</svg>
