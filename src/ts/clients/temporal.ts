79c36c9ce4f6bb6a92acc65ecfd497b8c20ba036
Saar Sayfan <saarsayfan@gmail.com>
2022-03-06 16:33:41 -0800

    add NuDate class to temporal

--- src/ts/clients/temporal.ts
+++ src/ts/clients/temporal.ts
@@ -1,5 +1,163 @@
 const FIRST_AAD_YEAR = 2016;
 
+type FullNuDateFormat = 'y-m-d' | 'm/d/y';
+type PartialNuDateFormat = 'm d';
+type NuDateFormat = FullNuDateFormat | PartialNuDateFormat;
+
+type NuDateRelativeDistance = `${number}d` | `${number}w` | `${number}m` | `${number}y`;
+type NuDateRelativeRange = { start: NuDateRelativeDistance, end: NuDateRelativeDistance };
+
+class NuDate extends Date {
+    static translateFormattedString(
+        formattedString: string,
+        fromFormat: FullNuDateFormat,
+        toFormat: FullNuDateFormat
+    ): string | null {
+        let regExpString;
+        const dayExp = '(?<day>(3[01])|([12][0-9])|[1-9])';
+        const monthExp = '(?<month>(1[0-2])|[1-9])';
+        const yearExp = '(?<year>[0-9]{1,4})';
+        switch(fromFormat) {
+            case 'y-m-d':
+                regExpString = `${yearExp}-${monthExp}-${dayExp}`;
+                break;
+            case 'm/d/y':
+                regExpString = `${monthExp}/${dayExp}/${yearExp}`;
+                break;
+            default:
+                return null;
+        }
+        const regExp = new RegExp(regExpString);
+        const match = formattedString.match(regExp);
+        if (match === null) {
+            return null;
+        }
+        const {day, month, year} = match.groups;
+        return NuDate.getFormattedString(
+            parseInt(day),
+            parseInt(month) - 1,
+            parseInt(year),
+            toFormat
+        );
+    }
+
+    static getFormattedString(
+        day: number,
+        month: number,
+        year: number,
+        format: NuDateFormat
+    ): string {
+        return new NuDate(year, month - 1, day).toFormattedString(format);
+    }
+
+    static parseDateDistance(
+        distance: NuDateRelativeDistance
+    ): {number: number, unit: string, isNegative: boolean} {
+        const regExpString = '(?<number>-?[0-9]+)(?<unit>)[dmy]';
+        const regExp = new RegExp(regExpString);
+        const match = distance.match(regExp);
+        if (match === null) {
+            return null;
+        }
+        const rawNumber = match.groups.number;
+        return {
+            number: parseInt(rawNumber),
+            unit: match.groups.unit,
+            isNegative: rawNumber.startsWith('-'),
+        };
+    }
+
+    clone(): NuDate {
+        return new NuDate(this.getFullYear(), this.getMonth(), this.getDate());
+    }
+
+    isEqual(other: NuDate): boolean {
+        if (this.getFullYear() !== other.getFullYear()) {
+            return false;
+        }
+        if (this.getMonth() !== other.getMonth()) {
+            return false;
+        }
+        if (this.getDate() !== other.getDate()) {
+            return false;
+        }
+        return true
+    }
+
+    isLessThan(other: NuDate): boolean {
+        if (this.getFullYear() !== other.getFullYear()) {
+            return this.getFullYear() < other.getFullYear();
+        }
+        if (this.getMonth() !== other.getMonth()) {
+            return this.getMonth() < other.getMonth();
+        }
+        if (this.getDate() !== other.getDate()) {
+            return this.getDate() < other.getDate();
+        }
+        return true;
+    }
+
+    toFormattedString(format: NuDateFormat): string {
+        const day = this.getDate();
+        const month = this.getMonth() + 1;
+        const year = this.getFullYear();
+        switch(format) {
+            case 'y-m-d':
+                return `${year}-${month}-${day}`;
+            case 'm/d/y':
+                return `${month}/${day}/${year}`;
+            default:
+                return null;
+        }
+    }
+
+    getDateFromDistance(distance: NuDateRelativeDistance): NuDate | null {
+        const parsedDistance = NuDate.parseDateDistance(distance);
+        if (parsedDistance === null) {
+            return null;
+        }
+        const {number, unit, isNegative} = parsedDistance;
+        let newDate = this.clone();
+        switch(unit) {
+            case 'd':
+                newDate.setDate(newDate.getDate() + number);
+                break;
+            case 'w':
+                // adjust the current date to the first or last day of the week
+                if (isNegative) {
+                    newDate.setDate(newDate.getDate() - newDate.getDay());
+                } else {
+                    newDate.setDate(newDate.getDate() + (6 - newDate.getDay()));
+                }
+                newDate.setDate(newDate.getDate() + (7 * number));
+                break;
+            case 'm':
+                newDate.setMonth(newDate.getMonth() + number);
+                break;
+            case 'y':
+                newDate.setMonth(newDate.getMonth() + number);
+                break;
+        }
+        return newDate;
+    }
+
+    getRelativeDateRange(range: NuDateRelativeRange): NuDate[] {
+        const startDate = this.getDateFromDistance(range.start);
+        const endDate = this.getDateFromDistance(range.end);
+        const dateRange = [];
+        if (endDate.isLessThan(startDate)) {
+            return dateRange;
+        }
+        let dateIterator = startDate;
+        while (!dateIterator.isEqual(endDate)) {
+            dateRange.push(dateIterator);
+            dateIterator = dateIterator.clone();
+            dateIterator.setDate(dateIterator.getDate() + 1);
+        }
+        return dateRange;
+    }
+}
+
 function getDaysInMonth(month: number, year: number) {
     // returns the number of days in a month based on the year,
     // where month is 0-indexed. If the month is out of range,
@@ -61,6 +219,7 @@ function getDateString(d: Date) {
 }
 
 export {
+    NuDate,
     getDaysInMonth,
     getDatesThisWeek,
     getDateString,
