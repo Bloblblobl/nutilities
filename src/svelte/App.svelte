b4b501248c6243a70c873cccf53bad129c3a9171
Saar Sayfan <saarsayfan@gmail.com>
2022-03-07 22:33:26 -0800

    move more css from App to global.css

--- src/svelte/App.svelte
+++ src/svelte/App.svelte
@@ -11,44 +11,17 @@
 <TopBar/>
 <main>
     <Route path="/">
-        <Grid rows={4} columns={5}/>
+        <Grid rows={4} columns={5} />
     </Route>
     <Route path="/search">
-        <SpotifySearch/>
+        <SpotifySearch />
     </Route>
     <Route path="/calendar">
-        <Calendar/>
+        <Calendar />
     </Route>
 </main>
 
 <style>
-    @import url('https://fonts.googleapis.com/css2?family=Lobster&display=swap');
-    @import url('https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap');
-
-    :root {
-        --topbar-height: 3.5rem;
-
-        --c-light-gray: #b5bec6;
-        --c-dark-gray: #2b2d42;
-        --c-yellow: #f4e04d;
-        --c-mint: #8be8cb;
-
-        --c-spotify-green: #1db954;
-        --c-spotify-light-green: #1ed761;
-        --c-spotify-black: #191414;
-    }
-
-    :global(html, body) {
-        height: 100%;
-        margin: 0;
-        width: 100%;
-    }
-
-    :global(html) {
-        color: white;
-        font-family: 'Rubik', cursive;
-    }
-
     main {
         background-color: var(--c-dark-gray);
         box-sizing: border-box;
